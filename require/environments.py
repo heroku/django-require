c65fc8551e9efdaf78aed8a792049ad18c05d67a
Howard Burrows <howard@heroku.com>
2014-02-18 15:36:03 -0800

    Update require resources to the latest; changed node reference for debug environment to relative

--- require/environments.py
+++ require/environments.py
@@ -27,7 +27,7 @@ class Environment(object):
 class NodeEnvironment(Environment):
     def args(self):
         # Start of the command to run the compiler in Node.
-        return ["/app/vendor/node/bin/node"]
+        return ["node"] if require_settings.REQUIRE_DEBUG else ["/app/vendor/node/bin/node"]
 
 
 class RhinoEnvironment(Environment):
